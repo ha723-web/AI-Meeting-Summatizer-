28558d63a441878c0df8fd09494dd0d775a50431
Harshini Akunuri <harshiniakunuri59@gmail.com>
2025-05-21 15:24:25 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,17 @@ import streamlit as st
 from summarizer import summarize_text
 from transcriber import transcribe_audio
 import tempfile
+import spacy
+import os
+
+# Automatically download spaCy model if missing
+try:
+    nlp = spacy.load("en_core_web_sm")
+except:
+    os.system("python -m spacy download en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
+
+
 # Set full-width layout
 st.set_page_config(layout="wide", page_title="AI Meeting Summarizer")
 
